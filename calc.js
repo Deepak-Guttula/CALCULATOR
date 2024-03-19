8c7dfbfcbe8db687e3ea1b62dbc2741796a78fb8
Deepak-Guttula <96063667+Deepak-Guttula@users.noreply.github.com>
2024-03-19 15:39:12 +0530

    Delete calc.js

--- calc.js
+++
@@ -1,6 +0,0 @@
-function addition()
-{
-    var a = parseInt(document.getElementById("n1").value);
-    var b = parseInt(document.getElementById("n2").value);
-    document.getElementById("res").textContent = a+b;
-}
