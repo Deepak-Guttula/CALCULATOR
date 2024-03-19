adff3625cbc894d9c33dfddddba22d91870e7b20
Deepak-Guttula <96063667+Deepak-Guttula@users.noreply.github.com>
2024-03-19 15:33:26 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,6 @@
+function addition()
+{
+    var a = parseInt(document.getElementById("n1").value);
+    var b = parseInt(document.getElementById("n2").value);
+    document.getElementById("res").textContent = a+b;
+}
